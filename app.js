f2170a3158145701d43e8f6a9a5c5905edd00439
Henry Barnacle <henrybarnacle@yahoo.com>
2018-03-29 13:02:38 -0400

    building api

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var config = require('./config');
 
 var setupController = require('./controllers/setupController');
 
+var apiController = require('./controllers/apiController');
+
 var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
@@ -15,6 +17,7 @@ app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
-setupController(app);
+
+apiController(app);
 
 app.listen(port);
--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 var configValues = require('./config');
 
 module.exports = {
+	
 	getDbConnectionString: function() {
 		return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds127589.mlab.com:27589/nodetodosample';
-		
+
 	}
 }
